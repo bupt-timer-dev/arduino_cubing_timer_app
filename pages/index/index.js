7cea40cb1bd66832a1b4ee8a7e466c063874edee
renfei147 <renfei8@live.cn>
2022-01-21 23:26:55 +0800

    Update UI

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,49 @@
 // index.js
 // 获取应用实例
 const app = getApp()
+
+function msToStr(ms) {
+  let minute = Math.floor(ms / 60000)
+  ms = ms % 60000
+  let str = (ms / 1000).toFixed(3)
+  if (ms < 10000) str = "0" + str
+  if (minute > 0) str = minute + ":" + str
+  return str
+}
 Page({
-  data: {},
-  go_software() {
-    wx.navigateTo({
-      url: '/pages/software_timing/software_timing'
+  millisecond: 0,
+  lastTime: null,
+  data: {
+    timeStr: "00.000",
+    buttonText: "开始",
+  },
+  toggle() {
+    if (this.intervalHandle) {
+      this.tick()
+      this.setData({
+        buttonText: "开始"
+      })
+      clearInterval(this.intervalHandle)
+      this.intervalHandle = null
+    } else {
+      this.setData({
+        buttonText: "暂停"
+      })
+      this.lastTime = Date.now();
+      this.intervalHandle = setInterval(this.tick, 11);
+    }
+  },
+  reset() {
+    this.millisecond = 0;
+    this.setData({
+      timeStr: "00.000",
     })
   },
-  connect() {
-
+  tick() {
+    this.millisecond += Date.now() - this.lastTime;
+    this.lastTime = Date.now();
+    this.setData({
+      timeStr: msToStr(this.millisecond)
+    })
   }
 })
