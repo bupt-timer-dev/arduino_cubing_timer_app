103d7c73985d1fae49bf3af3fcb10f6010eb60ee
renfei147 <renfei8@live.cn>
2022-01-22 23:15:57 +0800

    Update logic & add record

---
+++ models/records.js
@@ -0,0 +1,37 @@
+/*
+记录字段：
+type 魔方类型
+method 计时方式
+startTime 开始时间（时间戳）
+time 用时（毫秒）
+dnf 是否弃权
+*/
+let records = {
+  cache: null,
+  loadSync() {
+    const info = wx.getStorageInfoSync();
+    if ("records" in info.keys) {
+      const data = wx.getStorageSync("records");
+      records.cache = JSON.parse(data);
+    } else {
+      wx.setStorageSync("records", []);
+      records.cache = [];
+    }
+  },
+  save() {
+    wx.setStorage({
+      key: "records",
+      data: records.cache
+    })
+  },
+  push(record) {
+    records.cache.push(record);
+    records.save();
+  },
+  pop() {
+    records.cache.pop();
+    records.save();
+  }
+}
+records.loadSync();
+module.exports = records;
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,7 @@
 // index.js
 // 获取应用实例
 const app = getApp()
+const records = require("../../models/records.js")
 
 function msToStr(ms) {
   let minute = Math.floor(ms / 60000)
@@ -14,29 +15,62 @@ Page({
   millisecond: 0,
   lastTime: null,
   data: {
-    timeStr: "00.000",
-    buttonText: "开始",
+    state: "init", // init/run/stop
+    timeStr: "00.000"
   },
   toggle() {
-    if (this.intervalHandle) {
-      this.tick()
-      this.setData({
-        buttonText: "开始"
-      })
-      clearInterval(this.intervalHandle)
-      this.intervalHandle = null
+    if (this.data.state == "run") {
+      this.stop();
     } else {
-      this.setData({
-        buttonText: "暂停"
-      })
-      this.lastTime = Date.now();
-      this.intervalHandle = setInterval(this.tick, 11);
+      this.start();
     }
+  },
+  start() {
+    this.currentRecord = {
+      type: 0,
+      method: 0,
+      startTime: Date.now()
+    }
+    this.setData({
+      state: "run"
+    });
+    this.millisecond = 0;
+    this.startTicking();
+  },
+  stop() {
+    this.setData({
+      state: "stop"
+    });
+    this.stopTicking();
+    this.currentRecord.time = this.millisecond;
+    this.currentRecord.dnf = false;
+    records.push(this.currentRecord);
+  },
+  resume() {
+    records.pop();
+    this.setData({
+      state: "run"
+    });
+    this.startTicking();
+  },
+  deleteRecord() {
+    records.pop();
+    this.reset();
+  },
+  startTicking() {
+    this.lastTime = Date.now();
+    this.intervalHandle = setInterval(this.tick, 11);
+  },
+  stopTicking() {
+    this.tick()
+    clearInterval(this.intervalHandle);
+    this.intervalHandle = null;
   },
   reset() {
     this.millisecond = 0;
     this.setData({
       timeStr: "00.000",
+      state: "init"
     })
   },
   tick() {
